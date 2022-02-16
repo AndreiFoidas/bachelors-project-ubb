5ffc98437811b0c9a090a6586ac363ac5127e788
AndreiFoidas <62398994+AndreiFoidas@users.noreply.github.com>
2022-02-16 21:48:32 +0200

    finished first iteration of image recognition, not very smart

--- ImageClassification/main.py
+++ ImageClassification/main.py
@@ -1,18 +1,14 @@
+# import tensorflow
 
-#import tensorflow
-'''
-from tensorflow.keras.layers import Input, Lambda, Dense, Flatten,Dropout
+from tensorflow.keras.layers import Input, Lambda, Dense, Flatten, Dropout
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg19 import VGG19
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.applications.vgg19 import preprocess_input
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-'''
-from tensorflow.keras.layers import Input, Lambda, Dense, Flatten,Dropout
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.vgg19 import VGG19
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 import numpy as np
 import pandas as pd
@@ -20,13 +16,17 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
-
 # re-size all the images to this
 IMAGE_SIZE = [224, 224]
+train_path = "../Images/seven_plastics/train"
+test_path = "../Images/seven_plastics/test"
+res_path = "../Images/seven_plastics/manual_test"
+#checkpoint_path = "models/cp.ckpt"
+checkpoint_path = "models/cp2.ckpt"
+#checkpoint_path = "models/cpGray.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
 
 def Preprocessing_Images():
-    train_path = "../Images/seven_plastics/train"
-    test_path = "../Images/seven_plastics/test"
 
     x_train = []
 
@@ -37,6 +37,7 @@ def Preprocessing_Images():
             image_path = sub_path + "/" + img
             img_arr = cv2.imread(image_path)
             img_arr = cv2.resize(img_arr, (224, 224))
+            #img_arr = cv2.cvtColor(img_arr, cv2.COLOR_BGR2GRAY)
             x_train.append(img_arr)
 
     x_test = []
@@ -48,10 +49,14 @@ def Preprocessing_Images():
             image_path = sub_path + "/" + img
             img_arr = cv2.imread(image_path)
             img_arr = cv2.resize(img_arr, (224, 224))
+            #img_arr = cv2.cvtColor(img_arr, cv2.COLOR_BGR2GRAY)
             x_test.append(img_arr)
 
     train_x = np.array(x_train)
     test_x = np.array(x_test)
+
+    #train_x = np.repeat(train_x[..., np.newaxis], 3, -1)
+    #test_x = np.repeat(test_x[..., np.newaxis], 3, -1)
 
     print(train_x.shape)
     print(test_x.shape)
@@ -84,8 +89,7 @@ def Preprocessing_Images():
     print(train_y.shape)
     print(test_y.shape)
 
-
-def Model_Training():
+    # def Model_Training(): #-------------------------------------------------------------------
     vgg = VGG19(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
 
     # do not train the pre-trained layers of VGG-19
@@ -94,7 +98,7 @@ def Model_Training():
 
     x = Flatten()(vgg.output)
     # adding output layer.Softmax classifier is used as it is multi-class classification
-    prediction = Dense(3, activation='softmax')(x)
+    prediction = Dense(4, activation='softmax')(x)
 
     model = Model(inputs=vgg.input, outputs=prediction)
 
@@ -107,8 +111,108 @@ def Model_Training():
     # view the structure of the model
     print(model.summary())
 
+    early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
+    # Early stopping to avoid overfitting of model
+
+    cp_callback = ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
+    # Create a callback that saves the model's weights
+
+    # fit the model
+    history = model.fit(
+        train_x,
+        train_y,
+        validation_data=(test_x, test_y),
+        epochs=3,
+        callbacks=[early_stop, cp_callback],
+        batch_size=32, shuffle=True)
+
+    # accuracies
+    plt.plot(history.history['accuracy'], label='train acc')
+    plt.plot(history.history['val_accuracy'], label='val acc')
+    plt.legend()
+    plt.savefig('vgg-acc-rps-1.png')
+    plt.show()
+
+    # loss
+    plt.plot(history.history['loss'], label='train loss')
+    plt.plot(history.history['val_loss'], label='val loss')
+    plt.legend()
+    plt.savefig('vgg-loss-rps-1.png')
+    plt.show()
+
+    os.listdir(checkpoint_dir)
+
+    for img in os.listdir(res_path):
+        img_name = img
+        img = image.load_img(res_path + "/" + img, target_size=(224, 224))
+        plt.imshow(img)
+        plt.show()
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        images = np.vstack([x])
+        pred = model.predict(images, batch_size=1)
+        print("Guessed val: " + str(img_name) + " " + str(pred))
+        if pred[0][0] > 0.5:
+            print("1 PET")
+        elif pred[0][1] > 0.5:
+            print("2 PE HD")
+        elif pred[0][2] > 0.5:
+            print("3 PVC")
+        elif pred[0][3] > 0.5:
+            print("5 ")
+        else:
+            print("Unknown")
+
+
+def Testing_Images():
+    # def Model_Training(): #-------------------------------------------------------------------
+    vgg = VGG19(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+    # do not train the pre-trained layers of VGG-19
+    for layer in vgg.layers:
+        layer.trainable = False
+
+    x = Flatten()(vgg.output)
+    # adding output layer.Softmax classifier is used as it is multi-class classification
+    prediction = Dense(4, activation='softmax')(x)
+
+    model = Model(inputs=vgg.input, outputs=prediction)
+
+    model.compile(
+        loss='sparse_categorical_crossentropy',
+        optimizer="adam",
+        metrics=['accuracy']
+    )
+
+    model.load_weights(checkpoint_path)
+
+    for img in os.listdir(res_path):
+        img_name = img
+        img = image.load_img(res_path + "/" + img, target_size=(224, 224))
+        plt.imshow(img)
+        plt.show()
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        images = np.vstack([x])
+        pred = model.predict(images, batch_size=1)
+        print("Guessed val: " + str(img_name) + " " + str(pred))
+        if pred[0][0] > 0.5:
+            print("1 PET")
+        if pred[0][1] > 0.5:
+            print("2 PE HD")
+        if pred[0][2] > 0.5:
+            print("3 PVC")
+        if pred[0][3] > 0.5:
+            print("5 ")
+
 
 if __name__ == '__main__':
-    Preprocessing_Images()
-
-
+    mode = 0
+    try:
+        mode = int(input('Input:'))
+    except ValueError:
+        print("Not a number")
+    if mode == 1:
+        Preprocessing_Images()
+    if mode == 2:
+        Testing_Images()
