c0254eb8c5886bd7e9183a271aacef24e30cf203
AndreiFoidas <62398994+AndreiFoidas@users.noreply.github.com>
2022-03-23 22:07:34 +0200

    created server, you can now send photos to the server to receive a result

---
+++ App/app/src/main/java/com/ubb/andrei/domain/Plastic.kt
@@ -0,0 +1,10 @@
+package com.ubb.andrei.domain
+
+data class Plastic (
+    var number: String,
+    var name: String,
+    var description: String,
+    var photoPath: String,
+    var reusable: Boolean,
+    var recyclable: Boolean
+)
