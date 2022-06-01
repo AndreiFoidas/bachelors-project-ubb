59201417a98d7b46ce200f913831650e8cab177a
AndreiFoidas <62398994+AndreiFoidas@users.noreply.github.com>
2022-06-02 00:34:19 +0300

    alpha of mobile app finished

--- App/app/src/main/java/com/ubb/andrei/InfoActivity.kt
+++ App/app/src/main/java/com/ubb/andrei/InfoActivity.kt
@@ -1,20 +1,31 @@
 package com.ubb.andrei
 
+import android.graphics.Color
+import android.graphics.Outline
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.ubb.andrei.domain.plasticList
 
 class InfoActivity : AppCompatActivity() {
 
     var recycleImageView: ImageView? = null
-    var textAbbreviation: TextView? = null
     var textName: TextView? = null
     var textDescription: TextView? = null
     var btnInfoBack: Button? = null
+    var mainLayout: LinearLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,26 +33,46 @@ class InfoActivity : AppCompatActivity() {
         val message = intent.getStringExtra(EXTRA_MESSAGE)
 
         recycleImageView = findViewById(R.id.recycleImageView)
-        textAbbreviation = findViewById(R.id.textAbbreviation)
         textName = findViewById(R.id.textName)
         textDescription = findViewById(R.id.textDescription)
         btnInfoBack  = findViewById(R.id.btnInfoBack)
+        mainLayout = findViewById(R.id.mainLayout)
 
-        var test = "Plastic: 0 TEST"
+
         val idFromMessage = message?.get(9)?.digitToInt()
-
         if (idFromMessage != null)
             if (idFromMessage in 1..8){
                 val plastic = plasticList.data[idFromMessage - 1]
 
-                textAbbreviation?.text = "${plastic.number} -  ${plastic.abbreviation}"
                 textName?.text = plastic.name
                 textDescription?.text = plastic.description
-                recycleImageView?.setBackgroundResource(plastic.photoPath)
+                recycleImageView?.setImageResource(plastic.photoPath)
+                mainLayout?.setBackgroundColor(Color.parseColor(plastic.backgroundColor))
             }
+
+        val card = findViewById<CardView>(R.id.eu)
+        val curveRadius = 80F
+
+        card.outlineProvider = object : ViewOutlineProvider() {
+
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun getOutline(view: View?, outline: Outline?) {
+                outline?.setRoundRect(0, 0, view!!.width, (view.height+curveRadius).toInt(), curveRadius)
+            }
+        }
+
+        card.clipToOutline = true
 
         btnInfoBack?.setOnClickListener {
             onBackPressed()
         }
+
+        val window: Window = this@InfoActivity.window
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        // finally change the color
+        window.statusBarColor = ContextCompat.getColor(this@InfoActivity, R.color.black)
     }
 }
--- App/app/src/main/java/com/ubb/andrei/domain/Plastic.kt
+++ App/app/src/main/java/com/ubb/andrei/domain/Plastic.kt
@@ -9,6 +9,7 @@ data class Plastic (
     var name: String,
     var description: String,
     var photoPath: Int,
+    var backgroundColor: String,
     var reusable: Boolean,
     var recyclable: Boolean
 )
@@ -26,41 +27,41 @@ fun initPlastics(): List<Plastic>{
             "the risk of the release of harmful substances, sometimes even carcinogenic. Plastic of this type is difficult to disinfect, requiring dangerous chemicals.\n" +
             "PET is a type of recyclable plastic, in the process the plastic is crushed and then crushed into small flakes which are then reprocessed to create other products. \n\n" +
             "Category 1 (PET) plastic products should be recycled, but not reused.",
-        R.drawable.p1, reusable = false, recyclable = true)
+        R.drawable.p1f, "#8fb35b",reusable = false, recyclable = true)
     plasticList.add(plastic)
     //2 HDPE
     plastic = Plastic(2, "HDPE", "High-density polyethylen",
             "HDPE is a rigid type of plastic from which detergent cans, toys and some bags are made, it is the most frequently recycled type of plastic and is considered one of the " +
             "least dangerous forms of plastic. This type of plastic is durable and not affected by direct sunlight, so it is used to make picnic tables, trash cans, benches and other " +
             "products that require durability. \n\nHDPE products are reusable and recyclable.",
-        R.drawable.p2, reusable = true, recyclable = true)
+        R.drawable.p2f, "#7284c2", reusable = true, recyclable = true)
     plasticList.add(plastic)
     //3 PVC
     plastic = Plastic(3, "PVC", "Polyvinyl chloride",
     "PVC is a type of soft and flexible plastic used for food packaging, for the production of toys for children and animals, computer cables and other products. This type of " +
             "plastic is also called \"poisonous plastic\" because it contains many toxins that it releases throughout its life cycle. \n\n" +
             "PVC products are not recyclable, and many of them are not reusable.",
-        R.drawable.p3, reusable = false, recyclable = false)
+        R.drawable.p3f, "#5bb2c5", reusable = false, recyclable = false)
     plasticList.add(plastic)
     //4 LDPE
     plastic = Plastic(4, "LDPE", "Low-density polyethylene",
     "LDPE is a type of plastic often found in heat-insulated packaging, in clothes bags from cleaners, in shopping bags, but also in some clothes and furniture. This type of " +
             "plastic is considered less toxic than others, but it is not normally recycled, as the products resulting from LDPE recycling are not very resistant. \n\n" +
             "LDPE products are reusable but not recyclable.",
-        R.drawable.p4, reusable = true, recyclable = false)
+        R.drawable.p4f, "#ce607e", reusable = true, recyclable = false)
     plasticList.add(plastic)
     //5 PP
     plastic = Plastic(5, "PP", "Polypropylene",
     "PP is a type of hard and light plastic that has excellent heat resistance qualities. It is used to pack food (to keep it fresh), to make lids for cans, margarine and " +
             "yogurt containers and straws. \n\n PP products are considered safe for reuse, but they are only recycled under certain conditions.",
-        R.drawable.p5, reusable = true, recyclable = false)
+        R.drawable.p5f, "#dc5252", reusable = true, recyclable = false)
     plasticList.add(plastic)
     //6 PS
     plastic = Plastic(6, "PS", "Polystyrene",
     "\n" +
             "PS is a cheap, lightweight plastic with a wide variety of uses: from disposable glasses and cutlery and egg boxes, to building materials. The products of this material " +
             "can release, especially as a result of their heating, carcinogenic substances. \n\n PS products should be avoided where possible, as recycling is quite difficult."
-        , R.drawable.p6, reusable = false, recyclable = false)
+        , R.drawable.p6f, "#f2885b", reusable = false, recyclable = false)
     plasticList.add(plastic)
     //7 OTHER
     plastic = Plastic(7, "O", "Others",
@@ -68,12 +69,13 @@ fun initPlastics(): List<Plastic>{
             "Category 7 plastic products are made from a combination of other types of plastic or less commonly used plastic formulas (such as BPA, polycarbonate and LEXAN) and are " +
             "difficult to recycle because they do not fall into a fixed category. This type of plastic is used to make products such as bottles and machine parts. \n\n" +
             "Products in this category are difficult to recycle and are not recommended for reuse.",
-        R.drawable.p7, reusable = false, recyclable = false)
+        R.drawable.p7f, "#d9ae4a", reusable = false, recyclable = false)
     plasticList.add(plastic)
     //8 NOT PLASTIC
     plastic = Plastic(8, "NOT PLASTIC", "Plastic combined with other materials",
-        "", R.drawable.p8, reusable = false, recyclable = false)
+        "", R.drawable.p8f, "#43505f", reusable = false, recyclable = false)
     plasticList.add(plastic)
 
     return plasticList
 }
+
